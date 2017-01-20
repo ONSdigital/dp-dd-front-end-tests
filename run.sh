d505be21cfb1ea4081e79431c3f036815c632d5c
giri <giridharpn@gmail.com>
2017-01-20 14:26:01 +0000

    Changed tests to point to real or stub

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "running data discovery frontend tests"
-mvn clean test -Dbrowser=chrome -Dbase_url=https://discovery.onsdigital.co.uk/dd/ -Dbackend=stub -Dtest=**/UITests.*
+mvn clean test -Dbrowser=chrome -Dbase_url=https://discovery.onsdigital.co.uk/dd/ -Dbackend=real -Dtest=**/UITests.*
---
+++ run_local.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "running data discovery frontend tests"
+mvn clean test -Dbrowser=chrome -Dbase_url=http://localhost:20040 -Dbackend=stub -Dtest=**/UITests.*
