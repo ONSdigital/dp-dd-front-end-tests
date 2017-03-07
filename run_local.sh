5774b821e06bd3d190c6f390523778d48ace4f08
giri <giridharpn@gmail.com>
2017-03-07 12:00:55 +0000

    clean up

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "running data discovery frontend tests"
-mvn clean test -Dbrowser=chrome -Dbase_url=http://localhost:20000/dd -Dbackend=stub -Dtest=**/CPITests.java
+mvn clean test -P FrontEnd-Tests -Dbase_url=http://localhost:20000/dd
