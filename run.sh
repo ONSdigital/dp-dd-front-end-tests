b43c1fb1ea92ce0f476f1171cb8b8979d2696e82
giri <giridharpn@gmail.com>
2017-03-07 10:50:02 +0000

    updated run.sh to run tests locally

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "running data discovery frontend tests"
-mvn clean test -Dbackend=real -Dtest=**/ArmedForcesTests.java
-mvn clean test -Dbackend=real -Dtest=**/CPITests.java
-mvn clean test -Dbackend=real -Dtest=**/AnnualBusinessSurvey.java
+mvn clean test -P FrontEnd-Tests
+#mvn clean test -Dbackend=real -Dtest=**/CPITests.java
+#mvn clean test -Dbackend=real -Dtest=**/AnnualBusinessSurvey.java
